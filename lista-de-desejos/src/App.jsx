2af6d9bfba97c33910e15956680f4db22a73b19e
Igor Almeida <Igor6Almeida@hotmail.com>
2026-01-20 10:22:16 -0300

    Mais alterações na linguagem

--- lista-de-desejos/src/App.jsx
+++ lista-de-desejos/src/App.jsx
@@ -4,7 +4,7 @@ import Items from './components/Items'
 function App() {
 
   const items = [
-    {id: 1, name: 'Fone de ouvido', price:100},
+    {id: 1, name: 'Headset', price:100},
     {id: 2, name: 'Smartwatch', price:150},
     {id: 3, name: 'Notebook', price: 2000},
     {id: 4, name: 'Tablet', price: 900},
