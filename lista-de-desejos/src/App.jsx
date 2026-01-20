18d6d23b627a8d78543e885e31c7285399e726f1
Igor Almeida <Igor6Almeida@hotmail.com>
2026-01-20 10:21:39 -0300

    Alterando linguagem do projeto

--- lista-de-desejos/src/App.jsx
+++ lista-de-desejos/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Items from './components/Items'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const items = [
+    {id: 1, name: 'Fone de ouvido', price:100},
+    {id: 2, name: 'Smartwatch', price:150},
+    {id: 3, name: 'Notebook', price: 2000},
+    {id: 4, name: 'Tablet', price: 900},
+    {id: 5, name: 'Smartphone', price: 2500},
+  ]
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+      <div className='app'>
+        <h1 className='title'>WishLista</h1>
+        
+
+        {items.map((item) => (
+
+          <Items key={item.id} products={item}/>
+          
+        ))}
+
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
